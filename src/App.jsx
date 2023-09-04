72aa95fa3c0ed9cae0d6c58d9c2301ddcfcc0a28
Fgonzalez409 <fergonz409@gmail.com>
2023-09-04 14:19:17 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <h1>hi there</h1>
+    </>
+  )
+}
+
+export default App
